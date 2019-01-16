499bcc0108df9d0693def37cf4c0ceced23c5744
Reva Aleksandr <saniareva@gmail.com>
2019-01-16 22:26:14 +0200

    Initial Commit

---
+++ FirstTestProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstTestProject
+//
+//  Created by Alekandr on 16.01.19.
+//  Copyright © 2019 RevaAlekandrUA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
